6676988922b00137de26001c8c303c36f0d12d4e
borisich666 <i.topchiev161@gmail.com>
2025-12-26 21:09:19 +0300

    startapp authenticate

---
+++ app/authenticate/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AuthenticateConfig(AppConfig):
+    name = 'authenticate'
+    verbose_name = 'Authentication'
+    verbose_name_plural = 'Authentication'
